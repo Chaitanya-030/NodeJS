a1d0f94df7afccb229eda9d4baa91157ac26a8e1
chaitanya <csheth845@gmail.com>
2023-12-13 00:15:46 +0530

    new product functionality added

--- e-dashboard/backend/index.js
+++ e-dashboard/backend/index.js
@@ -1,12 +1,19 @@
-const express = require('express');
-require('./db/Config');
-const cors = require('cors');
-const User = require("./db/User");
+const express = require('express'); // import express
 const app = express();
+
+require('./db/Config'); // import config file which connects the database
+
+const cors = require('cors'); // for specific error
+
+// import our collections
+const User = require("./db/User");
+const Product = require("./db/Product");
+
 
 app.use(express.json());
 app.use(cors());
 
+// backend for register
 app.post("/register", async (request, response) => {
     let user = new User(request.body)
     let result = await user.save();
@@ -15,6 +22,7 @@ app.post("/register", async (request, response) => {
     response.send(result);
 })
 
+// backend for login
 app.post('/login', async (request, response) => {
     if(request.body.password && request.body.email) {
         let user = await User.findOne(request.body).select("-password");
@@ -27,8 +35,15 @@ app.post('/login', async (request, response) => {
     }
     else{
         response.send({result: 'User Not Found'})
-    }
-    
+    }   
 })
+
+// backend for add product
+app.post('/addproduct', async (request, response) => {
+    let product = new Product(request.body);
+    let result = await product.save();
+    response.send(result);
+})
+
 
 app.listen(5000);
